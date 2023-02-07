ce7467bf242dc874899d1c0055c76ca14798bb8d
김지하 <kimjiha1112@gmail.com>
2023-02-08 00:39:57 +0900

    feat : first wallet success

--- index.js
+++ index.js
@@ -5,5 +5,10 @@
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import AppHoc from './AppHoc';
+import './shim.js'; // 추가된 코드
 
 AppRegistry.registerComponent(appName, () => AppHoc);
+
+/**
+ * @format
+ */
