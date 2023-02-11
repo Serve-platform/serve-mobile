b0e71909cfac600aa3bb9215cf8a3c5f39763efc
김지하 <kimjiha1112@gmail.com>
2023-02-11 11:41:34 +0900

    feat: sigupView

--- src/screens/onBoard/SignUp.tsx
+++ src/screens/onBoard/SignUp.tsx
@@ -1,25 +1,55 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import React, { useState } from 'react';
 import Wallet from 'ethereumjs-wallet';
+import Logo from '../../assets/icons/logo.png';
+import { useNavigation } from '@react-navigation/native';
 
 const SignUp = () => {
+  const navigation = useNavigation();
   const [privateKey, setPrivateKey] = useState<string>();
   const [address, setAddress] = useState<string>();
-
+  console.log(privateKey, address, 'check wallet');
   const generateWallet = () => {
     const wallet = Wallet.generate();
     setPrivateKey('0x' + wallet.getPrivateKey().toString('hex'));
     setAddress('0x' + wallet.getAddress().toString('hex'));
+    navigation.navigate('TabNav');
   };
   return (
-    <View>
-      <Text onPress={generateWallet}>Generate Wallet</Text>
-      {privateKey && (
+    <View
+      style={{
+        alignItems: 'center',
+        backgroundColor: '#000000',
+        height: '100%',
+      }}>
+      <View style={{ height: 550 }}>
+        <Image
+          resizeMode="contain"
+          source={Logo}
+          style={{
+            width: 270,
+            height: 150,
+            top: 210,
+          }}
+        />
+      </View>
+      <TouchableOpacity
+        style={{
+          width: 300,
+          height: 60,
+          backgroundColor: '#D9D9D9',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onPress={generateWallet}>
+        <Text style={{ color: '#ABABAB' }}>새 지갑 만들기</Text>
+      </TouchableOpacity>
+      {/* {privateKey && (
         <View>
           <Text>Private Key: {privateKey}</Text>
           <Text>Address: {address}</Text>
         </View>
-      )}
+      )} */}
     </View>
   );
 };
