54c3fb5fd9bc0c20f4a423d68abba0082f933f5b
namkyu koo(구남규) <rnrb555@gmail.com>
2023-02-13 22:43:55 +0900

    feat: 안드로이드 메인 홈 화면 작업 (#4)

--- src/screens/onBoard/SignUp.tsx
+++ src/screens/onBoard/SignUp.tsx
@@ -1,8 +1,12 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import React, { useState } from 'react';
 import Wallet from 'ethereumjs-wallet';
-import Logo from '../../assets/icons/logo.png';
+import Logo from '~/assets/icons/logo.png';
+import kakao from '~/assets/icons/kakao.png';
+import onboarding from '~/assets/images/onboarding.png';
+import walletIcon from '~/assets/icons/wallet.png';
 import { GlobalProps } from '~/navigators/GlobalNav';
+import theme from '~/styles/color';
 
 const SignUp = ({ navigation }: GlobalProps) => {
   const [privateKey, setPrivateKey] = useState<string>();
@@ -17,38 +21,97 @@ const SignUp = ({ navigation }: GlobalProps) => {
   return (
     <View
       style={{
+        flex: 1,
+        display: 'flex',
         alignItems: 'center',
-        backgroundColor: '#000000',
-        height: '100%',
+        justifyContent: 'space-between',
+        backgroundColor: theme.color.black,
+        paddingHorizontal: 20,
       }}>
-      <View style={{ height: 550 }}>
-        <Image
-          resizeMode="contain"
-          source={Logo}
-          style={{
-            width: 270,
-            height: 150,
-            top: 210,
-          }}
-        />
-      </View>
-      <TouchableOpacity
+      <Image
+        resizeMode="contain"
+        source={Logo}
         style={{
-          width: 300,
-          height: 60,
-          backgroundColor: '#D9D9D9',
-          alignItems: 'center',
-          justifyContent: 'center',
+          marginTop: 120,
+          width: 270,
+          height: 150,
         }}
-        onPress={generateWallet}>
-        <Text style={{ color: '#ABABAB' }}>새 지갑 만들기</Text>
-      </TouchableOpacity>
-      {/* {privateKey && (
-        <View>
-          <Text>Private Key: {privateKey}</Text>
-          <Text>Address: {address}</Text>
-        </View>
-      )} */}
+      />
+
+      <Image
+        resizeMode="contain"
+        source={onboarding}
+        style={{
+          width: 244,
+          height: 236,
+        }}
+      />
+
+      <View style={{ width: '100%' }}>
+        <TouchableOpacity
+          style={{
+            paddingVertical: 16,
+            borderRadius: 5,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#FEE500',
+            display: 'flex',
+            flexDirection: 'row',
+          }}
+          onPress={() => {}}>
+          <Image
+            resizeMode="contain"
+            source={kakao}
+            style={{
+              marginRight: 15,
+              width: 20,
+              height: 19,
+            }}
+          />
+          <Text
+            style={{
+              color: theme.color.black,
+              fontSize: 17,
+              fontWeight: '500',
+            }}>
+            카카오톡으로 시작하기
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            marginTop: 10,
+            paddingVertical: 16,
+            borderRadius: 5,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderWidth: 1,
+            borderColor: theme.color.white,
+            display: 'flex',
+            flexDirection: 'row',
+          }}
+          onPress={generateWallet}>
+          <Image
+            resizeMode="contain"
+            source={walletIcon}
+            style={{
+              marginRight: 15,
+              width: 24,
+              height: 24,
+            }}
+          />
+          <Text
+            style={{
+              color: theme.color.white,
+              fontSize: 17,
+              fontWeight: '500',
+            }}>
+            지갑으로 시작하기
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={{ color: theme.color.white }}>© Team Serve</Text>
     </View>
   );
 };
