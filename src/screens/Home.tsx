1dcde3988f2d03b374607584ab21b384f4ff9dc7
김지하 <kimjiha1112@gmail.com>
2023-02-18 15:46:41 +0900

    FEAT : Qr code svg, scanner 화면, 모달 작업 (#5)
    * feat: 안드로이드 메인 홈 화면 작업
    
    * feat: link qr code
    - base 64 담은 qr svg생성
    qr scanner를 camera screen통해 생성
    - todo.. modal 끄기..
    
    ---------
    
    Co-authored-by: nain93 <rnrb555@gmail.com>

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -12,6 +12,18 @@ import React, { useEffect, useRef, useState } from 'react';
 import theme from '~/styles/color';
 import { onboarding } from '~/assets/images';
 import { downArrow } from '~/assets/icons';
+import { GlobalProps } from '../navigators/GlobalNav';
+import OnModal from '~/component/Home/OnModal';
+import OffModal from '~/component/Home/OffModal';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+
+const Home = ({ navigation }: GlobalProps) => {
+  const [onServe, setOnServe] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [OnmodalVisible, setOnModalVisible] = useState(false);
+  const moveAnim = useRef(new Animated.Value(-2)).current;
+  const [qrData, setQrData] = useState('');
 
 const Home = () => {
   const [onServe, setOnServe] = useState(false);
@@ -25,6 +37,44 @@ const Home = () => {
       useNativeDriver: true,
     }).start();
   };
+  const moveQr = () => {
+    setOnModalVisible(!OnmodalVisible);
+    navigation.navigate('QrScan');
+  };
+
+  const address__ = AsyncStorage.getItem('Address');
+  const balance__ = 1;
+  const apiUrl = `http://ec2-3-35-25-21.ap-northeast-2.compute.amazonaws.com:3000/api/users/createQr?address=${address__}&balance=${balance__}`;
+
+  console.log(onServe, modalVisible, OnmodalVisible, 'modalVisible');
+
+  async function postData(): Promise<void> {
+    axios
+      .get(apiUrl)
+      .then(response => {
+        setQrData(response.data.result);
+        console.log(qrData, 'qrdata');
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+  const moveQrCode = () => {
+    setModalVisible(!modalVisible);
+    postData();
+    navigation.navigate('QrScreen', {
+      qrData: qrData,
+    });
+  };
+  const moveOff = async () => {
+    Animated.timing(moveAnim, {
+      toValue: -2,
+      duration: 150,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+  };
+
 
   const moveOff = async () => {
     Animated.timing(moveAnim, {
@@ -110,6 +160,16 @@ const Home = () => {
           탑승 정보 입력
         </Text>
       </View>
+      {/* SERVE 드래그 버튼 */}
+      <View style={styles.dragContainer}>
+        <>
+          <Pressable
+            onPress={() => {
+              setOnServe(!onServe);
+              onServe
+                ? setModalVisible(!modalVisible)
+                : setOnModalVisible(!OnmodalVisible);
+            }}>
 
       {/* SERVE 드래그 버튼 */}
       <View style={styles.dragContainer}>
@@ -152,6 +212,12 @@ const Home = () => {
           />
           <View style={[styles.dragDisableButton, { bottom: -2, left: -2 }]}>
             <Text style={styles.dragDisableText}>Drag to SERVE</Text>
+          </View>
+          <View style={{ alignItems: 'center' }}>
+            <View style={{ alignItems: 'center' }}>
+              <OffModal offModalData={moveQrCode} modalVisible={modalVisible} />
+              <OnModal onModalData={moveQr} modalVisible={OnmodalVisible} />
+            </View>
           </View>
         </>
       </View>
