b1d2fbbac6387149018be7111dc44834ce22a32a
jiwon <ujw97@naver.com>
2023-03-01 21:32:25 +0900

    feat: Button type(white) 추가.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ type Props = {
   title: string;
   clickEvent?: () => void;
   style?: ViewStyle;
-  type?: 'default' | 'small' | 'yellow';
+  type?: 'default' | 'small' | 'yellow' | 'white';
 };
 const Button = ({ title, clickEvent, style, type = 'default' }: Props) => {
   const button = () => {
@@ -15,6 +15,8 @@ const Button = ({ title, clickEvent, style, type = 'default' }: Props) => {
         return <Text style={styles.small}>{title}</Text>;
       case 'yellow':
         return <Text style={styles.yellow}>{title}</Text>;
+      case 'white':
+        return <Text style={styles.white}>{title}</Text>;
       default:
         return <Text style={styles.default}>{title}</Text>;
     }
@@ -62,6 +64,20 @@ const styles = StyleSheet.create({
   yellow: {
     fontSize: 20,
     backgroundColor: theme.color.main,
+    color: theme.color.black,
+    borderColor: theme.color.black,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderRadius: 50,
+    borderRightWidth: 4,
+    borderBottomWidth: 6,
+    textAlign: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+  },
+  white: {
+    fontSize: 20,
+    backgroundColor: theme.color.white,
     color: theme.color.black,
     borderColor: theme.color.black,
     borderWidth: 1,
