4dea629f5e73a539a1e59c11195ba3ce7b313eec
nain93 <rnrb555@gmail.com>
2023-03-04 06:35:47 +0900

    feat: 열차 좌석 받아오는 query추가

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -20,7 +20,7 @@ import {
   seatIdState,
 } from '~/recoil/atoms';
 import { useMutation, useQuery } from 'react-query';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import DragButton from '~/components/DragButton';
@@ -214,7 +214,7 @@ const Home = () => {
                 fontSize: 20,
                 color: theme.color.white,
               }}>
-              {boardInfo.trainUuid !== ''
+              {isWatch
                 ? `${boardInfo.trainLocation}메트로 ${boardInfo.trainLine}호선 ${boardInfo.trainUuid}열차 ${boardInfo.doorNumber}호칸 탑승 중`
                 : '탑승 정보 입력'}
             </Text>
